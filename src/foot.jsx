68df90e855d6c54f653b44d5e93f99abe56afa15
BlueEnd07 <lokeshrawat5050@gmail.com>
2024-01-26 02:01:13 +0530

    fix git card

--- src/foot.jsx
+++ src/foot.jsx
@@ -5,7 +5,7 @@ const Foot=()=>{
       <h1 className="text-xl text-center font-mono font-thin w-full my4">Hi! its us devlopers of Minigeemu.This is where we make games and stuff for web</h1>
       <div id="footcard" className="flex justify-center w-full gap-3 my-4 hover:scale-110 transition delay-100 duration-300 ease-in-out">
         <div className="border-black border-2 rounded-md p-2 bg-gray-200 text-center flex justify-center" id="github">
-          <a className="flex gap-3" href="github.com">
+          <a className="flex gap-3" href="https://github.com/BlueEnd07/Minigeemu-react">
             <p className="text-l font-mono">Github</p>
             <img className="h-6" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="" />
           </a>

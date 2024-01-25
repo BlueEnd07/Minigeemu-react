93c9fc5c54a8db88da2f8cfe0747c35269ee4d8c
BlueEnd07 <lokeshrawat5050@gmail.com>
2024-01-26 01:53:08 +0530

    update url

--- src/cardcoll.jsx
+++ src/cardcoll.jsx
@@ -3,37 +3,42 @@ import Card from "./card";
 const Cardcoll = () => {
   const data = [
     {
-      url:"https://minigeemu-home.vercel.app/",
+      url:"https://password-game-pi.vercel.app/",
       imgsource: "./images/img1.webp",
       imgalt: "img1",
       gamename: "Name",
       gamdis: "something",
     },
     {
+      url:"https://game-folder.vercel.app/",
       imgsource: "./images/img2.webp",
       imgalt: "img2",
       gamename: "Name2",
       gamdis: "something2",
     },
     {
+      url:"https://pandaai.itch.io/dino-run",
       imgsource: "./images/img3.webp",
       imgalt: "img3",
       gamename: "Name3",
       gamdis: "something3",
     },
     {
+      url:"https://rock-paper-scissor-henna-alpha.vercel.app/",
       imgsource: "./images/img4.webp",
       imgalt: "img4",
       gamename: "Name4",
       gamdis: "something4",
     },
     {
+      url:"https://quiz-game-gamma-silk.vercel.app/",
       imgsource: "./images/img5.webp",
       imgalt: "img5",
       gamename: "Name5",
       gamdis: "something5",
     },
     {
+      url:"https://pandaai.itch.io/runner",
       imgsource: "./images/img6.webp",
       imgalt: "img6",
       gamename: "Name6",
